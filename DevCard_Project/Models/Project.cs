611d519197110c57d1c4ffd7384996cb5eba052c
abb155 <parnaz.behnam@gmail.com>
2024-05-25 00:03:43 +0330

    created ViewComponent for Project Page

---
+++ DevCard_Project/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿namespace DevCard_Project.Models
+{
+	public class Project
+	{
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public string Desciption { get; set; }
+		public string Image { get; set; }
+		public string Client { get; set; }
+
+		public Project(long id, string name, string desciption, string image, string client)
+		{
+			Id = id;
+			Name = name;
+			Desciption = desciption;
+			Image = image;
+			Client = client;
+		}
+	}
+}
---
+++ DevCard_Project/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using DevCard_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_Project.ViewComponents
+{
+	public class ProjectsViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<Project> { 
+			
+			new Project(1,"تاکسی","درخواست آنلاین برای سفرهای درون شهری","project-1.jpg","snap"),
+			new Project(2,"زودفود","تهیه غذای ایرانی و خارجی در سریعترین زمان","project-2.jpg","zodfood"),
+			new Project(3,"آموزشگاه","درخواست آموزشگاه آنلاین معتبر با اساتید معتبر ","project-3.jpg","online learn"),
+			new Project(4,"فضا پیما","آموزش مهارت های نجوم شناسی و سیارات","project-4.jpg","nasa"),
+			
+			
+			};
+			return View("_Projects",projects);
+		}
+	}
+}
